bf0289e8cab5e8f8c1131c87ae73ef84be197566
MnkyArts <liamrobinson5@web.de>
2023-09-27 22:22:49 +0200

    Added basic hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <h1 className='text-[#75ff69] mb-4 text-4xl lg:text-6xl font-extrabold'>Hey there!</h1>
+        <p className='text-white text-lg lg:text-xl'>
+            I'm Liam Robinson, a graphic designer and web developer from Lüneburg, Germany, with 10+ years of experience. I excel in logo, web, and print design, offering effective and efficient solutions for your design needs. Choose me for professional and skilled graphic design.
+        </p>
+    </section>
+  )
+}
+
+export default Hero
