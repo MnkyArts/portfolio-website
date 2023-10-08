dc85c8cd7d5e435f4e71d7367328084ebb328b0c
MnkyArts <liamrobinson5@web.de>
2023-10-08 15:41:21 +0200

    some css changes

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { getSortedPostsData } from "../../../lib/projects";
 import ProjectItem from "./ProjectItem";
 
@@ -7,6 +6,9 @@ export const Projects = () => {
   const projects = getSortedPostsData();
   return (
     <section className="mx-auto min-w-full px-24 py-20 lg:py-32 lg:px-52 border-b-2 border-b-[#e5e7eb1c] overflow-hidden relative">
+      <h2 className="z-10 text-white mb-5 lg:mb-24 text-4xl lg:text-6xl font-extrabold relative">
+        My latest Projects
+      </h2>
       <div className="absolute top-0 inset-x-0 h-[37.5rem] bg-gradient-to-b from-[#0a0e1a]"></div>
       <ul className="z-10 grid grid-cols-1 lg:grid-cols-4 relative">
         {projects.map((project) => (
@@ -14,7 +16,7 @@ export const Projects = () => {
         ))}
       </ul>
       <div className="absolute top-0 left-0 w-full h-full grid-pattern bg-top [mask-image:linear-gradient(0deg,transparent,black)]"></div>
-      <div className="absolute top-0 left-0 w-full h-full bg-[url('/img/projectbg.jpg')] bg-no-repeat bg-cover [mask-image:linear-gradient(-180deg,transparent,black,transparent)]"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-[url('/img/projectbg.jpg')] mix-blend-color bg-no-repeat bg-cover [mask-image:linear-gradient(-180deg,transparent,black,transparent)]"></div>
     </section>
   );
 };
--- src/app/projects/[projectId]/page.js
+++ src/app/projects/[projectId]/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import getFormattedDate from "../../../../lib/getFormattedDate";
+import Navbar from "@/app/components/Navbar";
 import { getSortedPostsData, getPostData } from "../../../../lib/projects";
 import { notFound } from "next/navigation";
 
@@ -33,10 +34,16 @@ export default async function Project({ params }) {
   const formattedDate = getFormattedDate(date);
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <div>{formattedDate}</div>
-      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
-    </div>
+    <main className="flex min-h-screen flex-col bg-[#0B1120]">
+      <Navbar />
+      <section className="mx-auto min-w-full px-24 py-20 lg:py-32 lg:px-52 overflow-hidden relative">
+        <h1 className="text-white text-1xl lg:text-2xl font-extrabold">
+          {title}
+        </h1>
+        <div className="text-green-400">{formattedDate}</div>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </section>
+    </main>
   );
 }
